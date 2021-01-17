8feacc1df429871252b76acf26031ee2e7736828
roridev <t3ctotalmenterandom1@outlook.com>
2021-01-17 15:21:09 -0300

    Adds samplec executable

---
+++ app/Main.hs
@@ -0,0 +1,37 @@
+module Main where
+
+import System.Environment
+import System.Directory
+import System.FilePath.Posix
+
+import Parser
+import Grammar
+import Compiler
+
+
+main :: IO ()
+
+main = do
+    args <- getArgs 
+    if null args then
+        putStrLn "Please input at least one file."
+    else
+        mapM_ stxToTex args
+
+
+stxToTex :: FilePath -> IO()
+stxToTex path = do
+    putStrLn $ "["++ path ++"] Compiling " ++ takeFileName path ++ "..."
+    contents <- readFile path
+    let unsafe = parseSampleTex contents
+    let safe = checkObjects unsafe
+    let tex = compile safe
+    createAndWriteFile ("./compiled/" ++ takeFileName path ++ ".tex") tex
+    putStrLn $ "Finished compiling " ++ takeFileName path
+
+
+createAndWriteFile :: FilePath -> String -> IO ()
+createAndWriteFile path content = do
+  createDirectoryIfMissing True $ takeDirectory path
+
+  writeFile path content
