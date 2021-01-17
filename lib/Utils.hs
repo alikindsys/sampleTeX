71ea1e40c1f5bf629a2e1c2eb3dde0579f6c22fa
roridev <t3ctotalmenterandom1@outlook.com>
2021-01-16 23:40:07 -0300

    Adds parseWithLeftOver

--- lib/Utils.hs
+++ lib/Utils.hs
@@ -1,10 +1,11 @@
 module Utils (parseBounded
              , parseUntil
              , parseEither
+             , parseWithLeftOver
              , Parser
              ) where
 
-import Text.Parsec ( satisfy, many1, (<|>) ) 
+import Text.Parsec ( satisfy, many1, manyTill, anyToken, eof, (<|>), ParseError, parse )
 import Text.Parsec.String (Parser)
 
 parseBounded :: (Char -> Bool) -> Parser String
@@ -16,3 +17,7 @@ parseUntil x = parseBounded (/= x)
 
 parseEither :: Parser a -> Parser b -> Parser (Either a b)
 parseEither pa pb = (Left <$> pa) <|> (Right <$> pb)
+
+parseWithLeftOver :: Parser a -> String -> Either ParseError (a,String)
+parseWithLeftOver p = parse ((,) <$> p <*> leftOver) ""
+   where leftOver = manyTill anyToken eof
