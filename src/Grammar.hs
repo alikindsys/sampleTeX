dc907026d36ef6422ccb46e9504338179332000a
roridev <t3ctotalmenterandom1@outlook.com>
2021-03-28 17:14:33 -0300

    Adds import store on documents

--- src/Grammar.hs
+++ src/Grammar.hs
@@ -8,24 +8,27 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 
 data Document
-    = SampleTex {variables :: Map String String, body :: [Either SimpleTexObject TexObject]}
+    = SampleTex {variables :: Map String String, imports :: Map ValidFile String , body :: [Either SimpleTexObject TexObject]}
+
+data ValidFile = Tex String 
+               | Sample String
+               deriving Show
 
 blankDocument :: Document
-blankDocument = SampleTex Map.empty []
+blankDocument = SampleTex Map.empty Map.empty []
 
 checkGrammar :: Document -> Either SimpleTexObject TexObject -> Document
 
-checkGrammar doc (Right b) = SampleTex (variables doc) (Right b : body doc)
+checkGrammar doc (Right b) = SampleTex  (variables doc) (imports doc) (Right b : body doc)
 
 checkGrammar doc (Left (Variable name value)) =
     if Map.notMember name (variables doc) then
-        SampleTex (Map.insert name value $ variables doc) $ body doc
+        SampleTex (Map.insert name value $ variables doc) (imports doc) $ body doc
     else
         error $ "Duplicated Variable Declaration : " ++ name
 
-
-checkGrammar doc (Left b) = SampleTex (variables doc) (Left b : body doc)
+checkGrammar doc (Left b) = SampleTex (variables doc) (imports doc) (Left b : body doc)
 
 checkObjects :: [Either SimpleTexObject TexObject] -> Document
-checkObjects x = SampleTex (variables doc) (reverse $ body doc)
+checkObjects x = SampleTex (variables doc) (imports doc) (reverse $ body doc)
     where doc = foldl checkGrammar blankDocument x
