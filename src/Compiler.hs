ae84185b828f1f1af91762d34c1ec9becfc5497e
roridev <t3ctotalmenterandom1@outlook.com>
2021-11-18 11:06:15 -0300

    Adds `DocumentState` data type

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -1,13 +1,26 @@
 module Compiler
   ( CompilationState (..),
+    DocumentState (..),
   )
 where
 
-import Parser (PathKind)
+import qualified Data.Map as M
+import Data.Map (Map)
+
+import Parser (PathKind, Identifier (..), StringLiteral (..))
 
 data CompilationState = CompilationState
   { _file :: String,
     _pwd :: String,
     _kind :: PathKind
+  }
+  deriving (Show)
+
+data DocumentState = DocumentState
+  { _variableMap :: Map Identifier StringLiteral,
+    _stack :: [String],
+    _hasClass :: Bool,
+    _initialized :: Bool,
+    _compilation :: CompilationState
   }
   deriving (Show)
