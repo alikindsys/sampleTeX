96fb58f7544e6f56e5948e4962799db574a3e2a4
roridev <t3ctotalmenterandom1@outlook.com>
2021-11-20 22:20:41 -0300

    Adds cli spec

--- app/Main.hs
+++ app/Main.hs
@@ -11,3 +11,16 @@ main = someFunc
 
 data SampleC = SampleC {files :: [FilePath], engine :: Maybe String}
   deriving (Show, Data, Typeable)
+
+spec =
+  SampleC
+    { files = def &= args &= typ "FILES",
+      engine = def &= help "The LaTeX engine used for PDF compilation"
+    }
+    &= summary "The SampleTex Compiler"
+    &= details
+      [ "samplec will compile to TeX by default",
+        "",
+        "to compile to PDF set your preferred LaTeX engine",
+        "and make sure its on %PATH%"
+      ]
