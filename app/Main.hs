2bd74c64e0e5ed85af17665004d0829555f2a14a
roridev <t3ctotalmenterandom1@outlook.com>
2021-11-20 22:19:51 -0300

    Adds `SampleC` data type

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Lib
+import System.Console.CmdArgs
 
 main :: IO ()
 main = someFunc
+
+data SampleC = SampleC {files :: [FilePath], engine :: Maybe String}
+  deriving (Show, Data, Typeable)
